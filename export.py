7bdd35f362690b962345b3c283c4536fbe295e47
Jason Benn <jasoncbenn@gmail.com>
2019-11-11 19:15:22 -0800

    Working scripts!

---
+++ export.py
@@ -0,0 +1,50 @@
+import os
+import sys
+
+from notion.block import ImageBlock
+from notion.client import NotionClient
+
+
+def get_page_url(page_id: str, page_title: str) -> str:
+    clean_title = "".join([c for c in page_title.replace(' ', '-') if c.isalnum() or c == '-'])
+    page_url = clean_title + page_id.replace('-', '')
+    return f"notion://www.notion.so/{page_url}"
+
+
+def make_card_from_page(page_id, page_title) -> str:
+    return f"{page_id};<a href='{get_page_url(page_id, page_title)}'>{page_title}</a>;"
+
+
+def make_image_card_from_page(page_id, page_title, image_url, compression) -> str:
+    return f"{page_id};<img src='{image_url}'>;<a href='{get_page_url(page_id, page_title)}'>{page_title}</a><br><p>{compression}</p>"
+
+
+token_v2 = "b9d79f1c69e7c498da57eba93e26d34903808c76ab90d2760d340f473db40a607c8325a393c84de178c044f9661b7cc4fe3efceadbe72b6755bf859764df46f103de466695b6673c0bd420c7a214"
+
+page_url = sys.argv[1]
+client = NotionClient(token_v2=token_v2)
+
+page = client.get_block(page_url)
+collection = client.get_collection(page.get('collection_id'))
+collection.refresh()
+rows = collection.get_rows()
+
+cards = []
+for row in rows:
+    images = [x for x in row.children if isinstance(x, ImageBlock)]
+    if not any(images):
+        continue
+    cards.append(make_image_card_from_page(row.id, row.title, images[0].source, row.get_all_properties()['compression']))
+    # cards.append(make_card_from_page(row.id, row.title))
+
+filename = page.title.lower().replace(' ', '-')
+
+storage_dir = "/Users/jasonbenn/.notion-to-anki"
+os.makedirs(storage_dir, exist_ok=True)
+with open(f"{storage_dir}/{filename}", "w") as f:
+    for card in cards:
+        print(card, file=f)
+
+
+import ipdb
+ipdb.set_trace()
---
+++ makefile
@@ -0,0 +1,11 @@
+SHELL := /usr/bin/env bash
+
+
+export_worldview:
+	python export.py https://www.notion.so/jasonbenn/d7a04baa1cea4dda983747b04ae3ddaa?v=727ed26317ec44caa4c9f2d8393a09b5
+
+export_people:
+	python export.py https://www.notion.so/jasonbenn/2f61537471d64420b40c263ea48ba9e8?v=823167eafe964ed096c24c0a038f5d2c
+
+export_thinks:
+	python export.py https://www.notion.so/jasonbenn/17213f11f7ef4814b06fe4966f446b91?v=7fee0e1edeb641f3adaebcf2d2e6d31d
