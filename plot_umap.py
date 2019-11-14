f7a35262c2d86a109c9acad0619bb160eb931d72
Jason Benn <jasoncbenn@gmail.com>
2019-11-14 10:25:50 -0800

    UMAP in Plotly.

--- plot_umap.py
+++ plot_umap.py
@@ -14,12 +14,13 @@ BASE_DIR = '/Users/jasonbenn/.notion-to-anki'
 df = pickle.load(open(f'{BASE_DIR}/outputs/df.pkl', 'rb'))
 vectors = list(df.vector.values)
 documents = list(df.document.unique())
-colors = sns.color_palette(n_colors=len(documents))
-color_map = dict(zip(documents, colors))
+color_palette = sns.color_palette(n_colors=len(documents))
+color_map = dict(zip(documents, color_palette))
+colors = [color_map[x] for x in df.document.values]
 
 embedding = reducer.fit_transform(vectors)
 
-plt.scatter(embedding[:, 0], embedding[:, 1], c=[color_map[x] for x in df.document.values])
+plt.scatter(embedding[:, 0], embedding[:, 1], c=colors)
 
 plt.gca().set_aspect('equal', 'datalim')
 plt.title('UMAP projection of Worldview', fontsize=24)
@@ -33,19 +34,18 @@ subprocess.call(["open", filepath])
 
 points_filepath = f"{dirpath}/{filename}.json"
 
-# import ipdb; ipdb.set_trace()
-
 xs, ys = zip(*embedding.tolist())
-points = {"x": xs, "y": ys,
-          "mode": 'markers+text',
-          "type": 'scatter',
-          "name": 'Team B',
-          "text": list(df.sentence.values),
-          "textfont": {
-              "family": 'Times New Roman'
-          },
-          "textposition": 'bottom center',
-          "marker": {"size": 12}
-          }
+points = {
+    "x": xs,
+    "y": ys,
+    "mode": 'markers',
+    "type": 'scatter',
+    "text": list(df.sentence.values),
+    "textfont": {
+        "family": 'Times New Roman'
+    },
+    "marker": {"color": colors},
+    "hoverinfo": "text"
+}
 
 open(points_filepath, 'w').write(json.dumps(points))
