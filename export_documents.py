e51ba5f7708a62fe0d828ce07bc5455455742415
Jason Benn <jasoncbenn@gmail.com>
2019-11-22 22:19:27 -0800

    Rename.

--- export_documents.py
+++ export_documents.py
@@ -1,12 +1,78 @@
-from notion.client import NotionClient
+import os
+import sys
+# from notion.block import ImageBlock
+from pathlib import Path
 
-from utils import to_markdown
+from notion.client import NotionClient
+from tqdm import tqdm
+
 from utils import to_plaintext
+
+
+def clean_title(string: str) -> str:
+    return "".join([c for c in string.replace(' ', '-') if c.isalnum() or c == '-']).lower()
+
+
+def get_page_url(page_id: str, page_title: str) -> str:
+    url = clean_title(page_title) + page_id.replace('-', '')
+    return f"notion://www.notion.so/{url}"
+
+
+def make_card_from_text_page(page_id, page_title) -> str:
+    return f"{page_id};<a href='{get_page_url(page_id, page_title)}'>{page_title}</a>;"
+
+
+def make_image_card_from_page(page_id, page_title, image_url, compression) -> str:
+    return f"{page_id};<img src='{image_url}'>;<a href='{get_page_url(page_id, page_title)}'>{page_title}</a><br><p>{compression}</p>"
+
+
+def remove_newlines(string: str) -> str:
+    return string.replace('\n', '<br>')
+
+
+def make_card_from_person_page(row) -> str:
+    full_name = row.first_name.split(' ')[0] + ' ' + row.last_name
+    next_question = remove_newlines(row.next_question_to_ask_them)
+    return f"{row.id};" \
+        f"<a href='{get_page_url(row.id, row.title)}'>{full_name}</a>;" \
+        f"<b>Compression:</b> {row.compression}<br>" \
+        f"<b>Next Q:</b> {next_question}<br>" \
+        f"<b>Groups:</b> {', '.join(row.groups)}<br>" \
+        f"<b>Location:</b> {', '.join(row.location)}<br>" \
+        f"<b>Edited:</b> {row.edited.strftime('%-m/%-d/%y')}<br>" \
+        f"<b>Added:</b> {row.added.strftime('%-m/%-d/%y')}"
+
+
+export_type = sys.argv[1]
+page_url = sys.argv[2]
+assert export_type in ["text", "people", "document"]
 
 token_v2 = open('token_v2').read().strip()
 client = NotionClient(token_v2=token_v2, monitor=True)
 
-worldview_url = "https://www.notion.so/jasonbenn/Flashcards-make-memory-a-choice-5f0f818c3cf846e288e638f3339881ce"
-page = client.get_block(worldview_url)
-thing = to_plaintext(page)
-import ipdb; ipdb.set_trace()
+page = client.get_block(page_url)
+rows = []
+for x in tqdm(page.views[0].get()['page_sort']):
+    rows.append(client.get_block(x))
+
+storage_dir = Path("/Users/jasonbenn/.notion-to-anki")
+docs_path = storage_dir / "documents"
+os.makedirs(docs_path, exist_ok=True)
+
+cards = []
+for row in tqdm(rows):
+    if export_type == "people":
+        # images = [x for x in row.children if isinstance(x, ImageBlock)]
+        # if not any(images):
+        #     continue
+        cards.append(make_card_from_person_page(row))
+    elif export_type == "text":
+        cards.append(make_card_from_text_page(row.id, row.title))
+    elif export_type == "document":
+        open(docs_path / row.id, 'w').write(to_plaintext(row))
+
+
+filename = page.title.lower().replace(' ', '-')
+with open(storage_dir/filename, "w") as f:
+    for card in cards:
+        print(card, file=f)
--- makefile
+++ makefile
@@ -2,16 +2,16 @@ SHELL := /usr/bin/env bash
 
 # Commands API: Notion to Anki
 export_worldview:
-	python export_to_anki.py text https://www.notion.so/jasonbenn/d7a04baa1cea4dda983747b04ae3ddaa?v=727ed26317ec44caa4c9f2d8393a09b5
+	python export_documents.py text https://www.notion.so/jasonbenn/d7a04baa1cea4dda983747b04ae3ddaa?v=727ed26317ec44caa4c9f2d8393a09b5
 
 export_people:
-	python export_to_anki.py people https://www.notion.so/jasonbenn/2f61537471d64420b40c263ea48ba9e8?v=823167eafe964ed096c24c0a038f5d2c
+	python export_documents.py people https://www.notion.so/jasonbenn/2f61537471d64420b40c263ea48ba9e8?v=823167eafe964ed096c24c0a038f5d2c
 
 export_questions:
-	python export_to_anki.py text https://www.notion.so/jasonbenn/17213f11f7ef4814b06fe4966f446b91?v=7fee0e1edeb641f3adaebcf2d2e6d31d
+	python export_documents.py text https://www.notion.so/jasonbenn/17213f11f7ef4814b06fe4966f446b91?v=7fee0e1edeb641f3adaebcf2d2e6d31d
 
 export_processes:
-	python export_to_anki.py text https://www.notion.so/jasonbenn/130ffb8be05b4d5aaa9445c863760035?v=bced35625f4e4a06a1e82046f763d429
+	python export_documents.py text https://www.notion.so/jasonbenn/130ffb8be05b4d5aaa9445c863760035?v=bced35625f4e4a06a1e82046f763d429
 
 export_all:
 	make -j 3 export_questions export_people export_worldview export_processes
@@ -19,7 +19,7 @@ export_all:
 
 # Commands API: UMAP
 export_documents:
-	python export_to_anki.py document https://www.notion.so/jasonbenn/d7a04baa1cea4dda983747b04ae3ddaa?v=727ed26317ec44caa4c9f2d8393a09b5
+	python export_documents.py document https://www.notion.so/jasonbenn/d7a04baa1cea4dda983747b04ae3ddaa?v=727ed26317ec44caa4c9f2d8393a09b5
 
 vectorize_documents:
 	python vectorize.py
