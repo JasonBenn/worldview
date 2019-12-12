9d850d66fe6068d9cdd8f5f0f761c64d01f410d9
Jason Benn <jasoncbenn@gmail.com>
2019-12-12 14:41:35 -0800

    Change colors, symbols.

--- web/views.py
+++ web/views.py
@@ -1,10 +1,12 @@
 import json
 
-import seaborn as sns
 from django.shortcuts import render
 
 from web.models import Document
 from web.models import NotionDatabase
+
+INBOX_SYMBOL = 100
+DONE_SYMBOL = 200
 
 
 def index(request):
@@ -16,15 +18,20 @@ def index(request):
         *[(x.projection[0], x.projection[1], x.source.notiondocument.parent_database_id, x.text) for x in
           Document.objects.all().select_related('source__notiondocument')])
 
-    db_ids = NotionDatabase.objects.values_list('id', flat=True).order_by('id')
-    color_palette = sns.color_palette(n_colors=len(db_ids))
-    color_map = dict(zip(db_ids, color_palette))
-    colors = [color_map[x] for x in document_db_ids]
+    # db_ids = NotionDatabase.objects.values_list('id', flat=True).order_by('id')
+    worldview_db_id = NotionDatabase.objects.get(title="Worldview").id
+    # color_palette = sns.color_palette(n_colors=len(db_ids))
+    # color_map = dict(zip(db_ids, color_palette))
+    # colors = [color_map[x] for x in document_db_ids]
 
     context = {
         "xs": json.dumps(xs),
         "ys": json.dumps(ys),
         "texts": json.dumps(texts),
-        "colors": json.dumps(colors)
+        "colors": json.dumps(document_db_ids),
+        "symbols": json.dumps([DONE_SYMBOL if x == worldview_db_id else INBOX_SYMBOL for x in document_db_ids]),
+        "color_min": NotionDatabase.objects.order_by('id').first().id,
+        "color_max": NotionDatabase.objects.order_by('id').last().id,
     }
+
     return render(request, 'index.html', context)
