3c43e12f408d52a4acd0962d4862154ca136c55d
Jason Benn <jasoncbenn@gmail.com>
2020-03-22 18:26:42 -0700

    Dashboard project: download roam backups from S3, build a basic graph of notes.

--- web/management/commands/build_dashboard.py
+++ web/management/commands/build_dashboard.py
@@ -1,63 +1,67 @@
+import json
+import os
 import re
-from argparse import FileType
 from datetime import datetime
-from sys import stdin
 from typing import List
 
-import attr
-from dateutil import parser
-from dateutil.parser import ParserError
+from django.contrib.admin.utils import flatten
 from django.core.management import BaseCommand
+from django.utils.functional import partition
 from jinja2 import Template
+from networkx import Graph
 
-DATA_DIR = "Users/jasonbenn/data/Roam-Export-1584772783629"
+
+def dfs(node):
+    if 'children' not in node:
+        return
+    yield from node['children']
+    for child in node['children']:
+        yield from dfs(child)
 
 
-@attr.s
-class Note:
-    title: str = attr.ib()
-    dates: List[datetime] = attr.ib()
-    tags: List[str] = attr.ib()
+LINK_REGEX = re.compile('(?:\[\[.*\]\]|#[\w\d]+)')
 
-    @classmethod
-    def from_line(cls, raw_line: str) -> "Note":
-        line = raw_line.lstrip(DATA_DIR)
-        note_title, note_str = line.split(':', 1)
-        # Title might be a date, or the note title.
-        title = note_title.rstrip(".md")
-        tags = re.findall("(#[\w\d]+)", note_str)
-        try:
-            date = parser.parse(title)
-            # If that worked, this is a one-line note fragment.
-            title = note_str.lstrip("- ")
-            dates = [date]
-        except ParserError:
-            dates = [parser.parse(x.lstrip('#')) for x in tags if any([str(y) in x for y in [2019, 2020, 2021]])]
 
-        return cls(title, dates, tags)
+def parse_links(string: str) -> List[str]:
+    matches = re.findall(LINK_REGEX, string)
+    return [x.lstrip('#').lstrip('[[').rstrip("]]") for x in matches]
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        parser.add_argument('input', nargs='?', type=FileType('r'), default=stdin)
-
     def handle(self, *args, **kwargs):
-        # Read the zip files' mtime, make a note of when this was last updated.
+        backup_filepath = "data/roam-backup/jason.json"
+        data = json.loads(open(backup_filepath, "r").read())
+        graph = Graph()
 
-        # lines = [x for x in kwargs['input'].read().split('\n') if x]
-        # notes = [Note.from_line(x) for x in lines]
-        # print(notes[:3])
+        # Create nodes
+        for node in data:
+            if not ('children' in node and len(node['children'])):
+                continue
 
+            tagged_children = [x for x in node['children'] if '#Note' in x['string']]
+            inline_children, node_descriptors = partition(lambda x: '::' in x['string'], tagged_children)
+
+            for inline_node in inline_children:
+                graph.add_node(inline_node['string'])
+
+            if len(node_descriptors):
+                graph.add_node(node['title'], data=node)
+
+        # Create edges
+        for node_title, node in graph.nodes(data=True):
+            if 'data' not in node:
+                continue
+            links = flatten([parse_links(x['string']) for x in dfs(node['data'])])
+            for link in [x for x in links if graph.has_node(x)]:
+                graph.add_edge(link, node_title)
+
+        # Count edges
+        for node_title, node in graph.nodes(data=True):
+            node['num_edges'] = len(graph[node_title])
+
+        # Build index.html
+        notes = list(graph.nodes(data=True))
         template = Template(open('web/templates/dashboard.html').read())
-
+        last_updated = datetime.fromtimestamp(os.path.getmtime(backup_filepath))
         with open("build/index.html", "w") as f:
-            f.write(template.render(test_value="allo", last_updated=datetime.today()))
-
-
-# /Users/jasonbenn/data/Roam-Export-1584772783629/March 20th, 2020.md:- #Notes workaholism can make sense when you own the value of your work. burnouts and inconsistent motivation are much bigger problems when you are working for someone else's benefit.
-
-# /Users/jasonbenn/data/Roam-Export-1584772783629/Intelligence is compression.md:- Tags:: #Notes #CS294 #AI
-
-# /Users/jasonbenn/data/Roam-Export-1584772783629/Why Gradient Clipping Accelerates Training.md:Tags:: #[[ML papers]] #Notes [[March 10th, 2020]] [[March 11th, 2020]]
-
-# /Users/jasonbenn/data/Roam-Export-1584772783629/AI research knowledge is organizational, not individual.md:- Tags:: #Tweets [[February 17th, 2020]] #Research #AI #[[Learning organizations]] [[Knowledge theory]] [[Organizational theory]] #Notes
+            f.write(template.render(notes=notes, last_updated=last_updated))
