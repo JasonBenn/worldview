e7788c1331b93b47d5e6efc6bd84cad95f1c9708
Jason Benn <jasoncbenn@gmail.com>
2020-03-22 19:35:18 -0700

    v1 of the dashboard!

--- web/management/commands/build_dashboard.py
+++ web/management/commands/build_dashboard.py
@@ -30,38 +30,46 @@ def parse_links(string: str) -> List[str]:
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         backup_filepath = "data/roam-backup/jason.json"
-        data = json.loads(open(backup_filepath, "r").read())
+        pages = json.loads(open(backup_filepath, "r").read())
         graph = Graph()
 
         # Create nodes
-        for node in data:
-            if not ('children' in node and len(node['children'])):
+        for page in pages:
+            if not ('children' in page and len(page['children'])):
                 continue
 
-            tagged_children = [x for x in node['children'] if '#Note' in x['string']]
-            inline_children, node_descriptors = partition(lambda x: '::' in x['string'], tagged_children)
+            for page_type in ["Question", "Reference", "Note", "Post"]:
+                matches = [x for x in page['children'] if '#' + page_type in x['string']]
 
-            for inline_node in inline_children:
-                graph.add_node(inline_node['string'])
+                content, metadata = partition(lambda x: '::' in x['string'], matches)
 
-            if len(node_descriptors):
-                graph.add_node(node['title'], data=node)
+                for line in content:
+                    graph.add_node(line['string'], type=page_type)
+
+                if len(metadata):
+                    graph.add_node(page['title'], data=page, type=page_type)
 
         # Create edges
-        for node_title, node in graph.nodes(data=True):
-            if 'data' not in node:
+        for title, data in graph.nodes(data=True):
+            if 'data' not in data:
                 continue
-            links = flatten([parse_links(x['string']) for x in dfs(node['data'])])
+            links = flatten([parse_links(x['string']) for x in dfs(data['data'])])
             for link in [x for x in links if graph.has_node(x)]:
-                graph.add_edge(link, node_title)
+                graph.add_edge(link, title)
 
         # Count edges
-        for node_title, node in graph.nodes(data=True):
-            node['num_edges'] = len(graph[node_title])
+        for title, data in graph.nodes(data=True):
+            data['num_edges'] = len(graph[title])
 
         # Build index.html
-        notes = list(graph.nodes(data=True))
+        nodes = list(graph.nodes(data=True))
+        questions = [(title, data) for title, data in nodes if data['type'] == 'Question']
+        references = [(title, data) for title, data in nodes if data['type'] == 'Reference']
+        notes = [(title, data) for title, data in nodes if data['type'] == 'Note']
+        posts = [(title, data) for title, data in nodes if data['type'] == 'Post']
+
         template = Template(open('web/templates/dashboard.html').read())
         last_updated = datetime.fromtimestamp(os.path.getmtime(backup_filepath))
-        with open("build/index.html", "w") as f:
-            f.write(template.render(notes=notes, last_updated=last_updated))
+
+        with open("dashboard/index.html", "w") as f:
+            f.write(template.render(questions=questions, references=references, notes=notes, posts=posts, last_updated=last_updated))
