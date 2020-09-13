22dd8e589868e5201786ba68e17ae0d3cf9aab3c
Jason Benn <jasoncbenn@gmail.com>
2020-09-13 09:23:05 -0700

    dotenv requires being in the worldview folder.

--- scripts/build_dashboard.sh
+++ scripts/build_dashboard.sh
@@ -3,8 +3,10 @@
 WORLDVIEW_PATH=/Users/jasonbenn/code/worldview
 BUCKET=jbenn-roam-backups/backups/
 KEY=`aws --profile jason s3 ls ${BUCKET} | sort | tail -n 1 | awk '{print $4}'`
-aws --profile jason s3 cp s3://${BUCKET}${KEY} ${WORLDVIEW_PATH}/data/roam-backup.zip
-rm ${WORLDVIEW_PATH}/data/roam-backup/*
-unzip -o ${WORLDVIEW_PATH}/data/roam-backup.zip -d ${WORLDVIEW_PATH}/data/roam-backup
-date > ${WORLDVIEW_PATH}/data/dashboard_built_at.txt
-/Users/jasonbenn/.pyenv/versions/3.7.4/envs/worldview/bin/python ${WORLDVIEW_PATH}/manage.py build_dashboard
+pushd ${WORLDVIEW_PATH}
+
+aws --profile jason s3 cp s3://${BUCKET}${KEY} ./data/roam-backup.zip
+rm ./data/roam-backup/*
+unzip -o ./data/roam-backup.zip -d ./data/roam-backup
+date > ./data/dashboard_built_at.txt
+/Users/jasonbenn/.pyenv/versions/3.7.4/envs/worldview/bin/python ./manage.py build_dashboard
--- scripts/build_dashboard_on_flock.sh
+++ scripts/build_dashboard_on_flock.sh
@@ -4,7 +4,7 @@ WORLDVIEW_PATH=/home/flock/worldview
 date > ${WORLDVIEW_PATH}/data/dashboard_built_at.txt
 pushd /home/flock/roam-notes
 git pull --quiet
-popd
 
+pushd ${WORLDVIEW_PATH}
 source /home/flock/venvs/worldview/bin/activate
-python ${WORLDVIEW_PATH}/manage.py build_dashboard
+python ./manage.py build_dashboard
